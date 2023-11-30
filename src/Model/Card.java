3005fa0597bcbba83685dacc69885f419d605b5e
Andres Guerola Crespo <you@example.com>
2023-11-30 13:02:41 +0100

    Card.java.finished

--- src/Model/Card.java
+++ src/Model/Card.java
@@ -4,5 +4,23 @@ public class Card {
     private int value;
     private String suit;
 
-    
+    public card(int value, String suit){
+        this.value = value;
+        this.suit = suit;
+    }
+    public Card(){
+        this(0,"");
+    }
+    public int getValue(){
+        return value;
+    }
+    public String getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return "Suit: " +suit+"" + value + '}';
+    }
 }
+
